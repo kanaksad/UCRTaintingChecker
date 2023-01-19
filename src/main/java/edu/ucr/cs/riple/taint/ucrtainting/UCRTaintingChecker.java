23900647583b41ef47581a7409735dc4cc95d417
Kanak Das <kdas006@ucr.edu>
2023-01-18 23:44:33 -0800

    Adds an option for annotated packages

--- src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
+++ src/main/java/edu/ucr/cs/riple/taint/ucrtainting/UCRTaintingChecker.java
@@ -2,10 +2,14 @@ package edu.ucr.cs.riple.taint.ucrtainting;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.StubFiles;
+import org.checkerframework.framework.source.SupportedOptions;
 
 /**
  * This is the entry point for pluggable type-checking.
  */
+@SupportedOptions({
+        UCRTaintingChecker.ANNOTATED_PACKAGES
+})
 @StubFiles({
         "Connection.astub",
         "apache.commons.io.astub",
@@ -23,4 +27,9 @@ import org.checkerframework.framework.qual.StubFiles;
         "Object.astub",
         "ProcessBuilder.astub"
 })
-public class UCRTaintingChecker extends BaseTypeChecker {}
+public class UCRTaintingChecker extends BaseTypeChecker {
+    /**
+     * If this option is supplied, use the package names as part of third party library code handling
+     */
+    public static final String ANNOTATED_PACKAGES = "annotatedPackages";
+}
